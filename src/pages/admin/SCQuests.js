8474112c1a9fd534f074d0eb1e18ca55ee748d62
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-04-06 18:24:19 +0800

    Improved SingleChoiceAdmin.

--- src/pages/admin/SCQuests.js
+++ src/pages/admin/SCQuests.js
@@ -3,18 +3,23 @@ import {
   Container,
   Divider,
   FormControl,
-  InputLabel,
   Grid,
+  InputLabel,
   Select,
   TextField,
   Typography,
+  InputAdornment,
+  OutlinedInput,
+  IconButton,
 } from "@material-ui/core";
 import { DataGrid } from "@material-ui/data-grid";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 import Header from "../../components/Header";
 import { addSCQuestion } from "../../reducers/singleChoiceReducer";
-import { v4 as uuidv4 } from "uuid";
+import CancelOutlinedIcon from "@material-ui/icons/CancelOutlined";
+import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
 
 const SCQuests = () => {
   const quests = useSelector((state) => state.singlechoices);
@@ -25,11 +30,12 @@ const SCQuests = () => {
     { field: "choice", headerName: "选项", width: 280 },
     { field: "correct", headerName: "正确答案", width: 200 },
   ];
+  
   const rows = quests.map((quest, arynum) => {
     return {
       id: arynum + 1,
       stem: quest.stem,
-      choice: quest.choice.join(),
+      choice: quest.choice.join(" / "),
       correct: quest.correct,
     };
   });
@@ -52,21 +58,45 @@ const SCQuests = () => {
 
 const AddSCQ = () => {
   const [stem, setStem] = useState("");
-  const [choice, setChoice] = useState([]);
+  const [stemErr, setStemErr] = useState(false);
+  const [choices, setChoices] = useState([""]);
   const [correct, setCorrect] = useState("");
+  const [correctErr, setCorrectErr] = useState(false);
   const [img, setImg] = useState("");
   const dispatch = useDispatch();
+
   const submitQuest = () => {
-    dispatch(addSCQuestion(stem, img, choice, correct));
+    //Detect all requirements
+    if (!stem && !correct) {
+      setStemErr(true);
+      setCorrectErr(true);
+    } else if (!stem) {
+      setStemErr(true);
+    } else if (!correct) {
+      setCorrectErr(true);
+    } else {
+      dispatch(addSCQuestion(stem, img, choices, correct));
+    }
   };
+
   const handleStemChange = (event) => {
     setStem(event.target.value);
+    if (event.target.value) {
+      setStemErr(false);
+    } else {
+      setStemErr(true);
+    }
   };
   const handleImgChange = (event) => {
     setImg(event.target.value);
   };
   const handleCorrectChange = (event) => {
     setCorrect(event.target.value);
+    if (event.target.value) {
+      setCorrectErr(false);
+    } else {
+      setCorrectErr(true);
+    }
   };
 
   return (
@@ -77,6 +107,8 @@ const AddSCQ = () => {
           <TextField
             label="题干"
             multiline
+            required
+            error={stemErr}
             variant="outlined"
             onChange={handleStemChange}
             style={{ width: "100%" }}
@@ -94,7 +126,12 @@ const AddSCQ = () => {
           />
         </Grid>
         <Grid item key="correct" lg={3} xs={12}>
-          <FormControl variant="outlined" style={{ width: "100%" }}>
+          <FormControl
+            required
+            error={correctErr}
+            variant="outlined"
+            style={{ width: "100%" }}
+          >
             <InputLabel htmlFor="fkyou">正确选项</InputLabel>
             <Select
               native
@@ -105,7 +142,7 @@ const AddSCQ = () => {
               value={correct}
             >
               <option value=""></option>
-              {choice.map((choice) => (
+              {choices.map((choice) => (
                 <option value={choice} key={uuidv4()}>
                   {choice}
                 </option>
@@ -114,16 +151,6 @@ const AddSCQ = () => {
           </FormControl>
         </Grid>
         <Grid item key="btn" lg={3} xs={12}>
-          <Button
-            onClick={() => {
-              setChoice(choice.concat(""));
-            }}
-            variant="contained"
-            color="secondary"
-            style={{ height: 56, width: "50%" }}
-          >
-            新增选项
-          </Button>
           <Button
             onClick={submitQuest}
             variant="contained"
@@ -135,40 +162,83 @@ const AddSCQ = () => {
         </Grid>
       </Grid>
       <Divider style={{ marginTop: 5, marginBottom: 5 }} />
-      <SCChoices choice={choice} setChoice={setChoice} />
+      <SCChoices choices={choices} setChoices={setChoices} />
     </>
   );
 };
 
-const SCChoices = ({ choice, setChoice }) => {
-  const changeChoices = (arynum, cgdchc) => {
-    const changedChoices = choice.map((choice, arynumb) => {
-      return arynumb === arynum ? cgdchc : choice;
+const SCChoices = ({ choices, setChoices }) => {
+  const changeChoices = (arynum, cgdchs) => {
+    const changedChoices = choices.map((choice, arynumb) => {
+      return arynumb === arynum ? cgdchs : choice;
     });
-    setChoice(changedChoices);
+    setChoices(changedChoices);
+  };
+  const deleteChoices = (arynum) => {
+    const changedChoices = [
+      ...choices.slice(0, arynum),
+      ...choices.slice(arynum + 1, choices.length),
+    ];
+    setChoices(changedChoices);
   };
   return (
     <Grid container spacing={2}>
-      {choice.map((choice, arynum) => {
+      {choices.map((choice, arynum) => {
         const handleChoiceChange = (event) => {
           const choiceToChange = arynum;
           const changedChoice = event.target.value;
           changeChoices(choiceToChange, changedChoice);
         };
+        const handleChoiceDelete = () => {
+          const choiceToDelete = arynum;
+          deleteChoices(choiceToDelete);
+        };
         return (
           <Grid item key={arynum} lg={3} xs={12}>
-            <TextField
-              label={`选项${arynum + 1}`}
-              multiline
+            <FormControl
               variant="outlined"
-              onChange={handleChoiceChange}
               style={{ width: "100%" }}
-              value={choice}
-            />
+              error={!choice}
+              required
+            >
+              <InputLabel>{`选项${arynum + 1}`}</InputLabel>
+              <OutlinedInput
+                multiline
+                onChange={handleChoiceChange}
+                value={choice}
+                labelWidth={42}
+                endAdornment={
+                  <InputAdornment position="end">
+                    {arynum !== choices.length - 1 ? (
+                      <IconButton onClick={handleChoiceDelete}>
+                        <CancelOutlinedIcon />
+                      </IconButton>
+                    ) : (
+                      <>
+                        {arynum !== 0 && (
+                          <IconButton onClick={handleChoiceDelete}>
+                            <CancelOutlinedIcon />
+                          </IconButton>
+                        )}
+                        <IconButton
+                          onClick={() => {
+                            setChoices(choices.concat(""));
+                          }}
+                        >
+                          <AddCircleOutlineOutlinedIcon />
+                        </IconButton>
+                      </>
+                    )}
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           </Grid>
         );
       })}
+      <Grid item key="add" lg={3} xs={12}></Grid>
     </Grid>
   );
 };
+
 export default SCQuests;
